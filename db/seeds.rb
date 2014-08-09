42e70065c85fecb91d4954ff8c92371429f80123
Elmer Wu <elmerwu@gmail.com>
2014-08-09 14:13:47 -0700

    adjust formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,18 @@
 json =ActiveSupport::JSON.decode(File.read('db/seeds/data.json'))
 
 json.each do |player|
-  Team.create(team_name: player["team"],
+  Team.create(
+              team_name: player["team"],
               city: player["city"],
               state: player["state"])
 
-  Player.create(name: player["name"],
+  Player.create(
+                name: player["name"],
                 position: player["position"],
                 stats: player["stats"],
                 rank: player["rank"],
                 image: player["image"],
                 age: player["age"],
-                team_id: Team.where(team_name: player["team"])[0]["id"]
-                )
+                team_id: Team.where(team_name: player["team"])[0]["id"])
 end
 
