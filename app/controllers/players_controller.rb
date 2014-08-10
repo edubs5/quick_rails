cde27aaac62171ca712759facf7c1b3cc908114b
Elmer Wu <elmerwu@gmail.com>
2014-08-09 17:29:07 -0700

    add individual player pages

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,13 @@
 class PlayersController < ApplicationController
+
   def index
     @teams = Team.all
     @players = Player.sorted
   end
+
+  def show
+    @player = Player.find(params[:id])
+    @team = Team.find(Player.find(params[:id]).team_id)
+  end
+
 end
